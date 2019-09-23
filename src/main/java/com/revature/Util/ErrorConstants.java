fb552223e01043038c101e0c3205e18b3697d8aa
shalujose <shalujosem0@gmail.com>
2019-09-23 22:57:52 +0530

    Changes committed

---
+++ src/main/java/com/revature/Util/ErrorConstants.java
@@ -0,0 +1,7 @@
+package com.revature.Util;
+
+public class ErrorConstants {
+
+	public static final String LOGIN_ERROR="Invalid data";
+	public static final String REGISTER_ERROR="Unable to register";
+}
